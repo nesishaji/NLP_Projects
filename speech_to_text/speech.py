821dcc57bba9a74885456a4df89010b8480ed549
Nisreen Punnassery <nisreenpunnassery@gmail.com>
2026-02-09 14:29:51 +0530

    Add files via upload

---
+++ speech_to_text/speech.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+r=sr.Recognizer()
+with sr.Microphone() as source:
+    print("Please speak...")
+    try:
+        audio = r.listen(source, timeout=5)
+        print(r.recognize_google(audio))
+    except sr.WaitTimeoutError:
+        print("You did not speak. Please speak.")
+    except sr.UnknownValueError:
+        print("I heard something, but could not understand. Please speak clearly.")
